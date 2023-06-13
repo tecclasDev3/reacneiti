b760de4d22e610e0a15e7eec67d1af1a014ef1f9
dev12Tecclas <129604075+dev12Tecclas@users.noreply.github.com>
2023-06-13 10:18:41 -0500

    primera version

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Image, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import axios from 'axios';
 
-export default function App() {
+const App = () => {
+  const [imageUri, setImageUri] = useState(null);
+
+  const selectImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Permisos insuficientes', 'Necesitas otorgar permisos para acceder a la biblioteca de medios');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      allowsMultipleSelection: true,
+    });
+
+    if (!result.canceled) {
+      const selectedImage = result.assets[0]; // Obtenemos la primera imagen seleccionada
+      setImageUri(selectedImage.uri);
+    }
+  };
+
+  const uploadImage = async () => {
+    try {
+      if (!imageUri) {
+        Alert.alert('Error', 'No se ha seleccionado ninguna imagen');
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('image', {
+        uri: imageUri,
+        type: 'image/jpeg',
+        name: 'image.jpg',
+      });
+
+      const response = await axios.post('http://example.com/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data);
+      Alert.alert('Éxito', 'La imagen se ha subido correctamente');
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error', 'Ocurrió un error al subir la imagen');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200, marginBottom: 20 }} />}
+      <Button title="Seleccionar imagen" onPress={selectImage} />
+      <Button title="Subir imagen" onPress={uploadImage} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
